7b14cd058e6b14c2b2fee4f2f4d6c53d711d9758
Sebastian Roth <sebastian.roth@gmail.com>
2013-07-01 10:37:48 +0800

    Switch Fade is a checkable menu item now.

--- sample/src/com/jfeinstein/jazzyviewpager/sample/MainActivity.java
+++ sample/src/com/jfeinstein/jazzyviewpager/sample/MainActivity.java
@@ -17,73 +17,82 @@ import com.jfeinstein.jazzyviewpager.OutlineContainer;
 
 public class MainActivity extends Activity {
 
-	private JazzyViewPager mJazzy;
+    static final String TOGGLE_FADE = "Toggle Fade";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		setupJazziness(TransitionEffect.Tablet);
-	}
+    private JazzyViewPager mJazzy;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		menu.add("Toggle Fade");
-		for (TransitionEffect effect : JazzyViewPager.TransitionEffect.values())
-			menu.add(effect.toString());
-		return true;
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupJazziness(TransitionEffect.Tablet);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getTitle().toString().equals("Toggle Fade")) {
-			mJazzy.setFadeEnabled(!mJazzy.getFadeEnabled());
-		} else {
-			TransitionEffect effect = TransitionEffect.valueOf(item.getTitle().toString());
-			setupJazziness(effect);
-		}
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem i = menu.add(TOGGLE_FADE);
+        i.setCheckable(true);
+        i.setChecked(mJazzy.getFadeEnabled());
 
-	private void setupJazziness(TransitionEffect effect) {
-		mJazzy = (JazzyViewPager) findViewById(R.id.jazzy_pager);
-		mJazzy.setTransitionEffect(effect);
-		mJazzy.setAdapter(new MainAdapter());
-		mJazzy.setPageMargin(30);
-	}
+        for (TransitionEffect effect : JazzyViewPager.TransitionEffect.values())
+            menu.add(effect.toString());
+        return true;
+    }
 
-	private class MainAdapter extends PagerAdapter {
-		@Override
-		public Object instantiateItem(ViewGroup container, final int position) {
-			TextView text = new TextView(MainActivity.this);
-			text.setGravity(Gravity.CENTER);
-			text.setTextSize(30);
-			text.setTextColor(Color.WHITE);
-			text.setText("Page " + position);
-			text.setPadding(30, 30, 30, 30);
-			int bg = Color.rgb((int) Math.floor(Math.random()*128)+64, 
-					(int) Math.floor(Math.random()*128)+64,
-					(int) Math.floor(Math.random()*128)+64);
-			text.setBackgroundColor(bg);
-			container.addView(text, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-			mJazzy.setObjectForPosition(text, position);
-			return text;
-		}
-		@Override
-		public void destroyItem(ViewGroup container, int position, Object obj) {
-			container.removeView(mJazzy.findViewFromObject(position));
-		}
-		@Override
-		public int getCount() {
-			return 10;
-		}
-		@Override
-		public boolean isViewFromObject(View view, Object obj) {
-			if (view instanceof OutlineContainer) {
-				return ((OutlineContainer) view).getChildAt(0) == obj;
-			} else {
-				return view == obj;
-			}
-		}		
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getTitle().toString().equals(TOGGLE_FADE)) {
+            mJazzy.setFadeEnabled(!mJazzy.getFadeEnabled());
+            item.setChecked(mJazzy.getFadeEnabled());
+        } else {
+            TransitionEffect effect = TransitionEffect.valueOf(item.getTitle().toString());
+            setupJazziness(effect);
+        }
+        return true;
+    }
+
+    private void setupJazziness(TransitionEffect effect) {
+        mJazzy = (JazzyViewPager) findViewById(R.id.jazzy_pager);
+        mJazzy.setTransitionEffect(effect);
+        mJazzy.setAdapter(new MainAdapter());
+        mJazzy.setPageMargin(30);
+    }
+
+    private class MainAdapter extends PagerAdapter {
+        @Override
+        public Object instantiateItem(ViewGroup container, final int position) {
+            TextView text = new TextView(MainActivity.this);
+            text.setGravity(Gravity.CENTER);
+            text.setTextSize(30);
+            text.setTextColor(Color.WHITE);
+            text.setText("Page " + position);
+            text.setPadding(30, 30, 30, 30);
+            int bg = Color.rgb((int) Math.floor(Math.random() * 128) + 64,
+                    (int) Math.floor(Math.random() * 128) + 64,
+                    (int) Math.floor(Math.random() * 128) + 64);
+            text.setBackgroundColor(bg);
+            container.addView(text, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+            mJazzy.setObjectForPosition(text, position);
+            return text;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object obj) {
+            container.removeView(mJazzy.findViewFromObject(position));
+        }
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            if (view instanceof OutlineContainer) {
+                return ((OutlineContainer) view).getChildAt(0) == obj;
+            } else {
+                return view == obj;
+            }
+        }
+    }
 }
